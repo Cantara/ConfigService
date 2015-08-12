317a8dc406294bb74ea62719df1fd56e63219678
Øystein Kjærnet <okj@capraconsulting.no>
2015-08-12 18:11:34 +0200

    GH-7: Added method that returns the parsed ServiceConfig instead of the raw response.

--- configservice-sdk/src/main/java/no/cantara/jau/serviceconfig/client/ConfigServiceClient.java
+++ configservice-sdk/src/main/java/no/cantara/jau/serviceconfig/client/ConfigServiceClient.java
@@ -8,10 +8,19 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.Base64;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import no.cantara.jau.serviceconfig.dto.ServiceConfig;
+import no.cantara.jau.serviceconfig.dto.ServiceConfigSerializer;
+
 /**
  * @author <a href="mailto:erik-dev@fjas.no">Erik Drolshammer</a> 2015-07-13.
  */
 public class ConfigServiceClient {
+
+    private static final Logger log = LoggerFactory.getLogger(ConfigServiceClient.class);
+
     public static String fetchServiceConfig(String url, String username, String password) throws IOException {
         URLConnection conn = new URL(url).openConnection();
         if (username != null && password != null) {
@@ -25,9 +34,21 @@ public class ConfigServiceClient {
             StringBuilder result = new StringBuilder();
             int c;
             while ((c = reader.read()) != -1) {
-                result.append((char)c);
+                result.append((char) c);
             }
             return result.toString();
         }
+    }
+
+    public static ServiceConfig fetchAndParseServiceConfig(String url, String username, String password)
+            throws IOException {
+
+        String response = fetchServiceConfig(url, username, password);
+        log.debug("Fetched ServiceConfig (length: {}).", response.length());
+
+        //Parse
+        ServiceConfig serviceConfig = ServiceConfigSerializer.fromJson(response);
+        log.debug("Parsed serviceConfig (timestamp: {})", serviceConfig.getChangedTimestamp());
+        return serviceConfig;
     }
 }
