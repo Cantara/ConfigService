6408e835b7b15a96c692722e46ad5604e944d6d0
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2015-10-12 09:58:56 +0200

    Commit of not working crontab solution of docker.

--- docker/toRoot/run-service.sh
+++ docker/toRoot/run-service.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-# this script make sure the main application is available and runs it
 
-APP=configservice.jar
-
-while [ ! -f "$APP" ]; do
-    echo "Fetching application from remote server"
-    ./update-service.sh
-
-    if [ ! -f "$APP" ]; then
-        >&2 echo "Retrieving application failed, trying again in 30 sec"
-        sleep 30
-    fi
-done
-
-/usr/bin/java -Dlogback.configurationFile=./logback.xml -jar "$APP"
+./update-service.sh
+crontab $HOME/crontab
+tail -f /var/log/cron.log
--- docker/toRoot/update-service.sh
+++ docker/toRoot/update-service.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Script to download deployment unit from a Maven artifact repository.
 
+APP=configservice.jar
+
 releaseRepo=http://mvnrepo.cantara.no/content/repositories/releases
 snapshotRepo=http://mvnrepo.cantara.no/content/repositories/snapshots
 groupId=no/cantara/jau
@@ -24,20 +26,39 @@ else #A specific Release version
    jarfile=$artifactId-$version.jar
 fi
 
-# Download artifact
-echo Downloading $url
-wget -O $jarfile -q -N $url
 
-# Create symlink or replace existing sym link
-if [ -h $artifactId.jar ]; then
-   unlink $artifactId.jar
+shaUrl=url.sha1
+shaFromWeb=$(wget $shaUrl -q -O -)
+if [ -f $APP ]; then
+  localSha=$(sha1sum $jarfile | awk '{print $1}')
+else
+  echo "No local app file found"
+  localSha=-1
 fi
-ln -s $jarfile $artifactId.jar
 
-# Delete old jar files
-jar=$artifactId*.jar
-nrOfJarFilesToDelete=`ls $jar -A1t | tail -n +6 | wc -l`
-if [[ $nrOfJarFilesToDelete > 0 ]]; then
-    echo Deleting $nrOfJarFilesToDelete old jar files. Keep the 4 newest + the symlink.
-    ls $jar -A1t | tail -n +6 | xargs rm
+if [ "$shaFromWeb" == "$localSha" ]; then
+  echo "Already got newest version. Not doing anything."
+else
+  echo "stopping running app, if it is running"
+  pkill -f '/usr/bin/java -Dlogback.configurationFile=./logback.xml -jar "$APP"&' # this doesn't work
+  echo Downloading $url
+  wget -O $jarfile -q -N $url
+
+  # Create symlink or replace existing sym link
+  if [ -h $artifactId.jar ]; then
+     unlink $artifactId.jar
+  fi
+  ln -s $jarfile $artifactId.jar
+
+  # Delete old jar files
+  jar=$artifactId*.jar
+  nrOfJarFilesToDelete=`ls $jar -A1t | tail -n +6 | wc -l`
+  if [[ $nrOfJarFilesToDelete > 0 ]]; then
+      echo Deleting $nrOfJarFilesToDelete old jar files. Keep the 4 newest + the symlink.
+      ls $jar -A1t | tail -n +6 | xargs rm
+  fi
+  
+  echo "Starting $APP"
+  /usr/bin/java -Dlogback.configurationFile=./logback.xml -jar "$APP"&
+
 fi
