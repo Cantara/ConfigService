86ed6b0309b7642068fc3c40b1188a53ae658e80
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2015-11-11 17:01:29 +0100

    now uses  in run-service.sh

--- Dockerprod/toRoot/run-service.sh
+++ Dockerprod/toRoot/run-service.sh
@@ -4,7 +4,7 @@ APP=configservice.jar
 JAVA_PARAMS="-Dlogback.configurationFile=./logback.xml -Xms128m -Xmx1024m"
 source config_override/service_override.properties # this might override JAVA_PARAMS and version to run
 
-START_APP_COMMAND="/usr/bin/java -Dlogback.configurationFile=./logback.xml -jar $APP"
+START_APP_COMMAND="/usr/bin/java $JAVA_PARAMS -jar $APP"
 
 ./update-service.sh
 
