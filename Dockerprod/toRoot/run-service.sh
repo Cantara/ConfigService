46e41f43e0b79a1768a3f7638fd73cc312c5057f
Anders Emil Rønning <andeero@ifi.uio.no>
2015-11-12 15:13:24 +0100

    Template for logback-override added

--- Dockerprod/toRoot/run-service.sh
+++ Dockerprod/toRoot/run-service.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 APP=configservice.jar
-JAVA_PARAMS="-Dlogback.configurationFile=./logback.xml -Xms128m -Xmx1024m"
+JAVA_PARAMS="-Dlogback.configurationFile=./logback-default.xml -Xms128m -Xmx1024m"
 source config_override/service_override.properties # this might override JAVA_PARAMS and version to run
 
 START_APP_COMMAND="/usr/bin/java $JAVA_PARAMS -jar $APP"
