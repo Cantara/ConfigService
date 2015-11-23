8b1d5d81e4c9942684ba4e7159094698374728eb
Anders Emil Rønning <andeero@ifi.uio.no>
2015-11-23 11:24:10 +0100

    Update with test script

--- Docker/test-docker.sh
+++ Docker/test-docker.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
+# This file will build a Docker image, create a data volume container and run an application container
+# from the image. Use it to test that your Docker configuration builds successfully.
 
-docker build -t configservice-dev .
+# Change these
+PROJECT_NAME=projectname
+IMAGE_NAME=docker-dev-test
+IMAGE_NAME_DATA=$IMAGE_NAME-data
 
-CONFIG_FILE="$(dirname $(pwd))/config_override.properties"
+docker rm -v -f $IMAGE_NAME
+docker build -t $IMAGE_NAME .
 
-if [ ! -f "$CONFIG_FILE" ]; then
-    echo "config_override.properties in parent directory is missing"
-    exit 1
-fi
+docker create --name $IMAGE_NAME-data $IMAGE_NAME
 
-LOCAL=""
-if [ "$1" == "local" ]; then
-    (cd .. && mvn package) || exit 2
-    JAR=$(cd ../target && ls -1t *.jar | head -1)
-    LOCAL="-v $(dirname $(pwd))/target/$JAR:/home/configservice/configservice.jar"
-fi
+docker run -d --name $IMAGE_NAME --volumes-from $IMAGE_NAME_DATA $IMAGE_NAME
 
-echo "Starting instance. Do 'docker exec -it configservice-dev bash' to get shell"
+echo "Starting instance. Do 'docker exec -it $IMAGE_NAME bash' to get shell"
 
-CONFIG="-v $CONFIG_FILE:/home/configservice/config_override.properties"
-docker run --rm -p 8086:8086 --name configservice-dev $CONFIG $LOCAL configservice-dev
 
