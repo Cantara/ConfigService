80100e7a2cf59cd9d4f057f875ce9b56e843462f
Anders Emil Rønning <andeero@ifi.uio.no>
2016-01-15 15:08:38 +0100

    Move event extraction classes from JAU to CS-sdk

---
+++ configservice-sdk/src/main/java/no/cantara/jau/serviceconfig/dto/event/ExtractedEventsStore.java
@@ -0,0 +1,38 @@
+package no.cantara.jau.serviceconfig.dto.event;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ExtractedEventsStore {
+    private final Map<String, EventGroup> eventGroups;
+
+    public ExtractedEventsStore() {
+        eventGroups = new HashMap<>();
+    }
+
+    public Map<String, EventGroup> getEventGroups() {
+        return eventGroups;
+    }
+
+    public void addEvents(List<Event> eventsToAdd) {
+        Map<String, List<Event>> eventsGroupedByGroup = eventsToAdd.stream()
+                .collect(Collectors.groupingBy(
+                        Event::getGroupName
+                ));
+
+        eventsGroupedByGroup.keySet().stream()
+                .forEach(groupName -> {
+                    List<Event> eventFilesToAdd = eventsGroupedByGroup.get(groupName);
+                    eventGroups.putIfAbsent(groupName, new EventGroup());
+                    EventGroup eventGroup = eventGroups.get(groupName);
+                    eventGroup.addEvents(eventFilesToAdd);
+                });
+    }
+
+    public EventGroup getEventGroup(String groupName) {
+        return eventGroups.get(groupName);
+    }
+
+}
