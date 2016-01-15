421c650429369bf2e096482e4e5c0f141746c9f5
Anders Emil Rønning <andeero@ifi.uio.no>
2016-01-15 15:16:07 +0100

    Make helper method static

--- configservice-sdk/src/main/java/no/cantara/jau/serviceconfig/client/EventExtractionUtil.java
+++ configservice-sdk/src/main/java/no/cantara/jau/serviceconfig/client/EventExtractionUtil.java
@@ -15,7 +15,7 @@ import static java.util.stream.Collectors.groupingBy;
 public class EventExtractionUtil {
     private static final Logger log = LoggerFactory.getLogger(EventExtractionUtil.class);
 
-    public Map<String, List<EventExtractionTag>> groupExtractionConfigsByFile(
+    public static Map<String, List<EventExtractionTag>> groupExtractionConfigsByFile(
             EventExtractionConfig config) {
         Map<String, List<EventExtractionTag>> collect = config.tags.stream()
                 .collect(groupingBy(item -> item.filePath));
